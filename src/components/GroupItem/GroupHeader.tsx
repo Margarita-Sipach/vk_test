6becf06b61f8a4db04e4a61205ecd8ada77b22b2
Margarita Sipach <margarita.sipach@gmail.com>
2024-03-11 15:42:01 +0300

    fix: increase avatar

--- src/components/GroupItem/GroupHeader.tsx
+++ src/components/GroupItem/GroupHeader.tsx
@@ -5,7 +5,7 @@ import { InitialsAvatarTextGradients } from "@vkontakte/vkui/dist/components/Ava
 export const GroupHeader = ({name, avatar_color}: GroupHeaderType) => {
     const initials = name.split(' ').map(i => i[0]).join('').toUpperCase()    
 
-return  <SimpleCell  before={<Avatar src="#" initials={initials} gradientColor={avatar_color as InitialsAvatarTextGradients} />}
+return  <SimpleCell  before={<Avatar size={100} src="#" initials={initials} gradientColor={avatar_color as InitialsAvatarTextGradients} />}
                     subtitle={closed ? 'закрытая' : 'открытая'}
         >
             {name}
